d34c7d883bb7e6636cd65a45dff11c4b4eef462f
tobby8629 <sarhmullah15@gmail.com>
2025-01-04 23:53:40 -0600

    fix animation on scroll to section

--- src/components/Sections/Highlights.jsx
+++ src/components/Sections/Highlights.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { playImg, rightImg, watchImg } from '../../utils'
+import { rightImg, watchImg } from '../../utils'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Highlights = () => {
-  useGSAP(()=>{
-    gsap.to('.links', {opacity: 1, stagger: 0.4})
-  },[])
+  useGSAP(() => {
+    gsap.to(".links", { 
+      opacity: 1, 
+      stagger: 0.4, 
+      scrollTrigger: {
+        trigger: ".links", 
+        start: "top 80%", 
+        end: "top 50%", 
+        toggleActions: "play none none none",
+      },
+    });
+  }, []);
   return (
-    <section className='bg-zinc common-padding w-screen overflow-hidden'>
+    <section className='bg-zinc h-screen common-padding w-screen overflow-hidden'>
       <div className='screen-max-width flex flex-wrap justify-between '>
         <h3 className='links section-heading font-normal'>Get the highlights.</h3>
         <div className='flex gap-7 items-end'>
